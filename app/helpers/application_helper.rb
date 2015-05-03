424aae3e755906e448215db985c24b2d2cd03a13
Skillvendor <Skillvendor193@gmail.com>
2015-05-03 15:28:20 +0300

    98 de bugguri pe perete , dai 1 jos esti fericit , 97 de bugguri pe perete...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	include Twitter::Autolink
 
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Honka"
     if page_title.empty?
       base_title
     else

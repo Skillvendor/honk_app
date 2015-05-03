65c78e4074f9cd957f791b0a76444d01ba23b7c3
Skillvendor <Skillvendor193@gmail.com>
2015-05-03 16:11:42 +0300

    97 de bugguri pe perete , dai 1 jos esti fericit , 95 de bugguri pe perete...

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,12 +16,12 @@ class TweetsController < ApplicationController
   end
 
   def destroy
-    @retweets = Tweet.where(id: @tweet.id)
+    @retweets = Tweet.where(original_tweet_id: @tweet.id)
 
     @retweets.each do |retweet|
       retweet.destroy
     end
-    
+
     @tweet.destroy
     flash[:success] = "Tweet deleted"
     redirect_to request.referrer || root_url
